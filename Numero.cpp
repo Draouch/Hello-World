3d00a786a34092dc10344ff03f88d31e1c0c83e0
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2015-09-04 09:06:45 -0500

    Numero.cpp

---
+++ Numero.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num;
+  cout<<"Ingresa un numero: "<<endl;
+  cin>>num;
+  if (num>50 and num<500)
+  cout<<"¡Ganaste!"<<endl;
+  else
+  cout<<"Perdiste"<<endl;
+  return 0;
+}
