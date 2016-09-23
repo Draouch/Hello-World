22b917e120960afa5835f89d945e6a5293ef99f8
Rodrigo - UCSP <Draouch@users.noreply.github.com>
2016-09-23 02:28:04 -0500

    Create print_multiarray.c

---
+++ Trabajo2/print_multiarray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+	
+	int s[5][2] = { {0,0},
+					{1,2},
+					{2,4},
+					{3,6},
+					{4,8}};
+	
+	int i, j;
+	
+	for ( i = 0; i < 5; i++ ) {
+		
+		for ( j = 0; j < 2; j++ ) {
+			printf("s[%d][%d] = %d\n", i, j, s[i][j] );
+		}
+	}
+	
+	return 0;
+}
